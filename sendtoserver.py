98ac10f384d28f9e0e6a8641d2413155957466b5
FlorenceYeedoun <130481462+FlorenceYeedoun@users.noreply.github.com>
2023-06-05 15:51:14 +0000

    Update sendtoserver.py

--- sendtoserver.py
+++ sendtoserver.py
@@ -2,6 +2,8 @@ import cv2
 import pytesseract
 import requests
 import json
+import os
+import subprocess
 
 # Function to process the image using Tesseract OCR
 def process_image(image):
@@ -9,49 +11,76 @@ def process_image(image):
     return text
 
 # Set the server API endpoint
-api_url = "http://your-server.com/api/upload"
+api_url = "https://traffic-offense.onrender.com/api/v1/offenses/raspberry"
 
-# Set the directory path to monitor
-directory_path = "/path/to/pi/directory"
-
-# Main loop to process images
-for file_name in os.listdir(directory_path):
-    # Read the image
-    image_path = os.path.join(directory_path, file_name)
-    image = cv2.imread(image_path)
-
-    # Resize the image (if needed)
-    new_width = 800
-    new_height = 600
-    resized_image = cv2.resize(image, (new_width, new_height))
-
-    # Process the resized image using Tesseract OCR
-    text = process_image(resized_image)
-
-    # Display the detected text
-    print("Image:", file_name)
-    print("Text:", text)
-    print("----------------------")
-
-    # Convert the image to base64 string
-    _, image_encoded = cv2.imencode(".jpg", resized_image)
-    image_base64 = image_encoded.tobytes()
-
-    # Create the payload for the API request
-    payload = {
-        "text": text,
-        "image": image_base64.decode("utf-8")
+# Define the violation types and their corresponding directories
+violation_types = {
+    "RED_LIGHT": "/home/pi/Red_Light_violation",
+    "SPEEDING": "/home/pi/Speed_violation"
+}
+# Define the violation amounts and currency
+fine = {
+    "RED_LIGHT": {
+        "amount": 500,
+        "currency": "GHS"
+    },
+    "SPEEDING": {
+        "amount": 320,
+        "currency": "GHS"
     }
+}
 
-    # Send the data to the server API endpoint
-    response = requests.post(api_url, json=payload)
+# Set the base directory path
+base_directory = "/home/pi"
 
-    # Handle the response from the server
-    if response.status_code == 200:
-        print("Data successfully sent to the server.")
-    else:
-        print("Failed to send data to the server.")
+# Main loop to process images from multiple directories
+for violation_type, directory_name in violation_types.items():
+    directory_path = os.path.join(base_directory, directory_name)
+    if not os.path.exists(directory_path):
+        continue
 
-    # Continue with the next image
+    for file_name in os.listdir(directory_path):
+        # Read the image
+        image_path = os.path.join(directory_path, file_name)
+        image = cv2.imread(image_path)
+
+        # Resize the image 
+        new_width = 800
+        new_height = 600
+        resized_image = cv2.resize(image, (new_width, new_height))
+
+        # Process the resized image using Tesseract OCR
+        text = process_image(resized_image)
+
+        # Display the detected text and violation type
+        print("Violation:", violation_type)
+        print("Image:", file_name)
+        print("Text:", text)
+        print("----------------------")
+
+        # Convert the image to base64 string
+        _, image_encoded = cv2.imencode(".jpg", resized_image)
+        image_base64 = image_encoded.tobytes()
+
+        # Create the payload for the API request
+        payload = {
+            "offense_name": violation_type,
+            "number_plate": text,
+            "image": image_base64,
+            "fine" : fine
+        }
+
+        # Send the data to the server API endpoint
+        response = requests.post(api_url, json=payload)
+
+        # Handle the response from the server
+        if response.status_code == 200:
+            print("Data successfully sent to the server.")
+        else:
+            print("Failed to send data to the server.")
+
+        # Continue with the next image
 
 cv2.destroyAllWindows()
+
+
