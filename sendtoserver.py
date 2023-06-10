d887530a318c80c54e26f235ebcf425eac687d97
FlorenceYeedoun <130481462+FlorenceYeedoun@users.noreply.github.com>
2023-06-10 17:55:13 +0000

    Update sendtoserver.py

--- sendtoserver.py
+++ sendtoserver.py
@@ -1,23 +1,17 @@
-import cv2
-import pytesseract
+import os
+import base64
 import requests
 import json
-import os
-import subprocess
-
-# Function to process the image using Tesseract OCR
-def process_image(image):
-    text = pytesseract.image_to_string(image)
-    return text
 
 # Set the server API endpoint
 api_url = "https://traffic-offense.onrender.com/api/v1/offenses/raspberry"
 
 # Define the violation types and their corresponding directories
 violation_types = {
-    "RED_LIGHT": "/home/pi/Red_Light_violation",
-    "SPEEDING": "/home/pi/Speed_violation"
+    "RED_LIGHT": "/home/flo/Red_Light_violation",
+    "SPEEDING": "/home/flo/Speed_violation"
 }
+
 # Define the violation amounts and currency
 fine = {
     "RED_LIGHT": {
@@ -42,33 +36,31 @@ for violation_type, directory_name in violation_types.items():
     for file_name in os.listdir(directory_path):
         # Read the image
         image_path = os.path.join(directory_path, file_name)
-        image = cv2.imread(image_path)
-
-        # Resize the image 
-        new_width = 800
-        new_height = 600
-        resized_image = cv2.resize(image, (new_width, new_height))
-
-        # Process the resized image using Tesseract OCR
-        text = process_image(resized_image)
-
-        # Display the detected text and violation type
-        print("Violation:", violation_type)
-        print("Image:", file_name)
-        print("Text:", text)
-        print("----------------------")
+        with open(image_path, 'rb') as image_file:
+            image_data = image_file.read()
 
         # Convert the image to base64 string
-        _, image_encoded = cv2.imencode(".jpg", resized_image)
-        image_base64 = image_encoded.tobytes()
+        image_base64 = base64.b64encode(image_data).decode('utf-8')
 
         # Create the payload for the API request
         payload = {
             "offense_name": violation_type,
-            "number_plate": text,
             "image": image_base64,
-            "fine" : fine
+            "file_name": file_name,
+            "fine": {
+                "amount": fine[violation_type]["amount"],
+                "currency": fine[violation_type]["currency"]
+            }
         }
+
+        # Print the payload
+        print("Payload:")
+        print(json.dumps(payload, indent=4))
+
+        # Save a copy of the image locally with the original file name
+        image_copy_path = os.path.join(base_directory, file_name)
+        with open(image_copy_path, 'wb') as image_copy_file:
+            image_copy_file.write(image_data)
 
         # Send the data to the server API endpoint
         response = requests.post(api_url, json=payload)
@@ -78,9 +70,6 @@ for violation_type, directory_name in violation_types.items():
             print("Data successfully sent to the server.")
         else:
             print("Failed to send data to the server.")
+            print("Response content:", response.content.decode())
 
         # Continue with the next image
-
-cv2.destroyAllWindows()
-
-
